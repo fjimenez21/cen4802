1d2dd9f093c203e3499b516186a4a58203445f03
fjimenez21 <112848862+fjimenez21@users.noreply.github.com>
2023-01-22 21:36:39 -0500

    Add files via upload

---
+++ FibonnacciMain.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FibonnacciMain {
+    public static int nthterm(int n){
+        if(n==0)return 0;
+        if(n==1 || n==2) return 1;
+        return nthterm(n-1)+nthterm(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = reader.nextInt();
+        System.out.println("The "+n+"th term of the sequence is "+nthterm(n));
+    }
+}
